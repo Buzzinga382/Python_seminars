3672ef93d8d18d43873455aa3ddd73dd35cb72f9
Anthony <gugl382@gmail.com>
2023-03-15 13:59:49 +0300

    Homework8 done

---
+++ Homework8/main.py
@@ -0,0 +1,71 @@
+# Задача 1. Напишите функцию read_last(lines, file), которая будет открывать
+# определенный файл file и выводить на печать построчно последние строки
+# в количестве lines (на всякий случай проверим, что задано положительное целое число).
+# Протестируем функцию на файле «article.txt» со следующим содержимым:
+#
+# Вечерело
+# Жужжали мухи
+# Светил фонарик
+# Кипела вода в чайнике
+# Венера зажглась на небе
+# Деревья шумели
+# Тучи разошлись
+# Листва зеленела
+
+# with open('article.txt', 'w', encoding='utf-8') as file:
+#     text = ['Вечерело', 'Жужжали мухи', 'Светил фонарик', 'Кипела вода в чайнике',
+#             'Венера зажглась на небе', 'Деревья шумели', 'Тучи разошлись', 'Листва зеленела']
+#     for el in text:
+#         file.write(el + '\n')
+#
+# def read_last(lines, file):
+#     with open(file, 'r', encoding='utf-8') as res:
+#         if lines < 0:
+#             print('Incorrect input!')
+#         text = res.read().splitlines()
+#         for i in range(len(text) - lines, len(text)):
+#             print(text[i])
+#
+# lines = int(input('Enter number of last lines: '))
+#
+# read_last(lines, 'article.txt')
+
+
+################################################################################
+
+
+# Задача 2. Документ «article.txt» содержит следующий текст:
+# Вечерело
+# Жужжали мухи
+# Светил фонарик
+# Кипела вода в чайнике
+# Венера зажглась на небе
+# Деревья шумели
+# Тучи разошлись
+# Листва зеленела
+# Требуется реализовать функцию longest_words(file), которая записывает в файл
+# result.txt слово, имеющее максимальную длину (или список слов, если таковых несколько).
+
+
+# def longest_words(file):
+#     with open(file, 'r', encoding='utf-8') as new_file:
+#         text = new_file.read().splitlines()
+#         new_list = []
+#         list2 = []
+#         res_list = []
+#         for i in range(len(text)):
+#             new_list.append(text[i].split())
+#         for j in new_list:
+#             for k in j:
+#                 list2.append(k)
+#         maxx = 0
+#         for el in list2:
+#             res_list.append(len(el))
+#         for num in range(len(res_list)):
+#             maxx = max(res_list)
+#         for elem in list2:
+#             if len(elem) == maxx:
+#                 with open('result.txt', 'a', encoding='utf-8') as res_file:
+#                     res_file.write(elem + '\n')
+#
+# longest_words('article.txt')
